9c139fd25083ca36b8c01ce1c7ce92d3e89067b4
EthanL1 <32692617+EthanL1@users.noreply.github.com>
2018-02-15 18:06:43 -0800

    v1
    Second Commit

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $stateInput = document.querySelector("#state");
 var $searchBtn = document.querySelector("#search");
 var $loadMoreBtn = document.querySelector("#load-btn");
+var $cityInput = document.querySelector("#city")
+var $countryInput = document.querySelector("#country")
+var $shapeInput = document.querySelector("#shape")
+var $stateInput = document.querySelector("#state");
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
-var filteredAddresses = dataSet;
+// Set filteredDataSet to dataSet initially
+var filteredDataSet = dataSet;
 
-// renderTable renders the filteredAddresses to the tbody
+// renderTable renders the filteredDataSet to the tbody
 function renderTable() {
   $tbody.innerHTML = "";
-  for (var i = 0; i < filteredAddresses.length; i++) {
-    // Get get the current address object and its fields
-    var address = filteredAddresses[i];
-    var fields = Object.keys(address);
+  for (var i = 0; i < filteredDataSet.length; i++) {
+    // Get get the current dataData object and its fields
+    var dataData = filteredDataSet[i];
+    var fields = Object.keys(dataData);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+      // For every field in the dataData object, create a new cell at set its inner text to be the current value at the current address's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
-      $cell.innerText = address[field];
+      $cell.innerText = dataData[field];
     }
   }
 }
@@ -32,16 +35,26 @@ function renderTable() {
 function handleSearchButtonClick() {
   // Format the user's search by removing leading and trailing whitespace, lowercase the string
   var filterState = $stateInput.value.trim().toLowerCase();
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  var filterCountry = $countryInput.value.trim().toLowerCase();
+  var filterShape = $shapeInput.value.trim().toLowerCase();
+  
+  // Set filteredDataSet to an array of all data whose "state, city, shape, country" matches the filter
+  filteredDataSet = dataSet.filter(function(dataData) {
+    var searchState = dataData.state.substring(0,filterState.length).toLowerCase();
+    var searchCountry = dataData.country.substring(0,filterCountry.length).toLowerCase();
+    var searchCity = dataData.city.substring(0,filterCity.length).toLowerCase();
+    var searchShape = dataData.shape.substring(0,filterShape.length).toLowerCase();
+    if ( searchState == filterState && searchCity == filterCity &&  
+      searchCountry == filterCountry && searchShape == filterShape){
+        return true;
+      }
+        return false;
+    });
+    renderTable();
+  }
+    
 
-  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-  filteredAddresses = dataSet.filter(function(address) {
-    var addressState = address.state.toLowerCase();
-
-    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-    return addressState === filterState;
-  });
-  renderTable();
-}
 
 // Render the table for the first time on page load
 renderTable();
